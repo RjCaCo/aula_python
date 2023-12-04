cb714202312ce97c09ff270501323bc43ea9a450
marceltsubota_ <marcel.tsu@hotmail.com>
2023-12-04 08:49:52 -0300

    teste git

--- codigo_01.py
+++ codigo_01.py
@@ -1,8 +1,8 @@
+print('Aula 1 - Python') 
 print('hello word') 
 print ("faturamento resultado", 42000 + 1000)
 btc = 42000
 btc_ontem  = 39800
 mensagem = "faturamento resultado"
-
 print (mensagem, btc - btc_ontem)
 
