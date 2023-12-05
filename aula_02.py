3cd24c8ef142922bda4c65f0e2ca2ceade8c5fe6
marceltsubota_ <marcel.tsu@hotmail.com>
2023-12-05 07:48:58 -0300

    operadores aritimeticos

--- aula_02.py
+++ aula_02.py
@@ -1,3 +1,30 @@
 name = input('Digite seu nome: ')
 language = input('Qual linguagem de programação você está estudando: ')
 print(name + ' está estudando ' + language)
+
+""" Operadores Aritméticos:
+'+ (adição)'
+'- (subtração)'
+'* (multiplicação)'
+'/ (divisão)'
+'% (módulo, retorna o resto da divisão)'
+'** (exponenciação)'
+'// (divisão inteira, retorna o quociente da divisão, sem parte fracionária) """
+
+a = 10
+b = 3
+
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b
+divisao = a / b
+resto = a % b
+exponenciacao = a ** b
+divisao_inteira = a // b
+
+print ("soma", soma)
+print ("subtracao", subtracao)
+print ("multiplicacao", multiplicacao)
+print ("divisao", divisao)
+print ("exponenciacao", exponenciacao)
+print ("divisao_inteira", divisao_inteira)
--- codigo_01.py
+++ codigo_01.py
@@ -1,6 +1,6 @@
 print('Aula 1 - Python') 
 print('hello word') 
-print ("faturamento resultado", 42000 + 1000)
+print ("faturamento resultado", 42000 ** 10)
 btc = 42000
 btc_ontem  = 39800
 mensagem = "faturamento resultado"
