093778c6e26f2cfa9bed37d83715b9d06684e108
marceltsubota_ <marcel.tsu@hotmail.com>
2023-12-06 07:16:59 -0300

    Continuação das estruturas de controle

--- aula_03.py
+++ aula_03.py
@@ -19,7 +19,43 @@ soma = 0
 for x in range(1, 11):
     soma += x
 
-print(f"A soma dos números de 1 a 10 é: {soma}")
-print("A soma dos números de 1 a 10 é:",soma)
+""" print(f"A soma dos números de 1 a 10 é: {soma}")
+print("A soma dos números de 1 a 10 é:",soma) """
 
 
+# Estruturas de Controle de Fluxo Iterativo (while, for)
+
+# break - Interrupção do Loop:
+
+contador = 0
+
+while True:  # Loop infinito
+    # print('exemplo 1: ', contador)
+    contador += 1
+
+    if contador >= 5:
+        break  # Interrompe o loop quando contador atinge 5
+
+# continue - Continuação para a Próxima Iteração:
+
+contador = 0
+
+while contador < 5:
+    contador += 1
+
+    if contador == 3:
+        continue  # Pula para a próxima iteração quando contador é igual a 3
+
+    # print('exemplo 2: ', contador)
+
+# Iteração com Listas:
+
+frutas = ['maçã', 'banana', 'laranja']
+
+""" for fruta in frutas:
+    print(fruta) """
+
+# Função range() para Geração de Sequências Numéricas:
+
+""" for i in range(5):
+    print(i) """
