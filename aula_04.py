4743a160522f1bd605b114ab1737692de0749fc1
marceltsubota_ <marcel.tsu@hotmail.com>
2023-12-06 08:24:09 -0300

    listas (ou array)

--- aula_04.py
+++ aula_04.py
@@ -1,8 +1,8 @@
 # Iteração com Listas:
 
-frutas = ['maçã', 'banana', 'laranja']
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-for fruta in frutas:
-    print(fruta)
-
-# Função range() para Geração de Sequências Numéricas:
+for numero in lista:
+    resto = numero % 2
+    if (resto == 0):
+        print(numero)
