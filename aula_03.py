be88a382d01cf6db28ea355394bcb096d956041e
marceltsubota_ <marcel.tsu@hotmail.com>
2023-12-06 07:24:44 -0300

    Inclusão de for simples

--- aula_03.py
+++ aula_03.py
@@ -1,5 +1,7 @@
 # ESTRUTURAS DE CONTROLE
 
+# Estruturas de Controle de Fluxo Condicional (if, else, elif)
+
 numero = 10
 
 """ if numero > 0:
@@ -10,22 +12,15 @@ else:
     print("Zero") """
 
 
-""" while numero < 20:
-    numero += 1
-    print("Número", numero) """
-
-soma = 0
-
-for x in range(1, 11):
-    soma += x
-
-""" print(f"A soma dos números de 1 a 10 é: {soma}")
-print("A soma dos números de 1 a 10 é:",soma) """
-
-
 # Estruturas de Controle de Fluxo Iterativo (while, for)
 
-# break - Interrupção do Loop:
+# While:
+
+while numero < 20:
+    numero += 1
+    # print("Número", numero)
+
+# Interrupção do Loop:
 
 contador = 0
 
@@ -36,7 +31,7 @@ while True:  # Loop infinito
     if contador >= 5:
         break  # Interrompe o loop quando contador atinge 5
 
-# continue - Continuação para a Próxima Iteração:
+# Continuação para a Próxima Iteração:
 
 contador = 0
 
@@ -47,6 +42,19 @@ while contador < 5:
         continue  # Pula para a próxima iteração quando contador é igual a 3
 
     # print('exemplo 2: ', contador)
+
+soma = 0
+
+# For
+
+""" for i in range(5):
+    print(i) """
+
+for x in range(1, 11):
+    soma += x
+
+# print(f"A soma dos números de 1 a 10 é: {soma}")
+# print("A soma dos números de 1 a 10 é:",soma)
 
 # Iteração com Listas:
 
